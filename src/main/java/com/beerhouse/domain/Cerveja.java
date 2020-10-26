9696cb58546399e567b72ee57514538e7365de1b
Renan Devenz <renan.devenz@hotmail.com>
2020-10-26 14:30:14 -0300

    Criação e ORM da entity Cerveja

---
+++ src/main/java/com/beerhouse/domain/Cerveja.java
@@ -0,0 +1,36 @@
+package com.beerhouse.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_CERVEJAS")
+public class Cerveja implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(name = "MARCA")
+    private String marca;
+
+    @NotBlank
+    @Column(name = "TIPO")
+    private String tipo;
+
+    @NotBlank
+    @Column(name = "QUANTIDADE")
+    private int quantidade;
+
+    @NotBlank
+    @Column(name = "PRECO")
+    private BigDecimal preco;
+}
