854465eb611579bbf47d7d6f9db02c678b59e465
Renan Devenz <renan.devenz@hotmail.com>
2020-10-26 15:48:44 -0300

    Implementação do CRUD

---
+++ src/main/java/com/beerhouse/service/CervejaService.java
@@ -0,0 +1,39 @@
+package com.beerhouse.service;
+
+import com.beerhouse.domain.Cerveja;
+import com.beerhouse.repository.CervejaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class CervejaService {
+
+    @Autowired
+    private CervejaRepository cervejaRepository;
+
+    public List<Cerveja> listar() {
+        return cervejaRepository.findAll();
+    }
+
+    public Optional<Cerveja> buscarPorId(Long id) {
+        return Optional.ofNullable(cervejaRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto inexistente")));
+    }
+
+    public Cerveja salvar(Cerveja cerveja) {
+        return cervejaRepository.save(cerveja);
+    }
+
+    public Cerveja atualizar(Cerveja cerveja, Long id) {
+        if (cervejaRepository.findById(id).isPresent()) {
+            return cervejaRepository.saveAndFlush(cerveja);
+        } throw new IllegalArgumentException("Não existe um produto para atualizar");
+    }
+
+    public void deletar(Long id) {
+        cervejaRepository.deleteById(id);
+    }
+}
