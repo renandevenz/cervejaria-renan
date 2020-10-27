17deceacc0b5c693ca264938dcb88d805714a3f5
Renan Devenz <renan.devenz@hotmail.com>
2020-10-27 11:37:45 -0300

    Implementação dos métodos Put e Delete

--- src/main/java/com/beerhouse/controller/CervejaController.java
+++ src/main/java/com/beerhouse/controller/CervejaController.java
@@ -4,16 +4,18 @@ import com.beerhouse.domain.Cerveja;
 import com.beerhouse.dto.CervejaDTO;
 import com.beerhouse.service.CervejaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/craftbeer")
+@RequestMapping("/craftbeer")
 public class CervejaController {
 
     @Autowired
@@ -32,9 +34,22 @@ public class CervejaController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping
     public ResponseEntity<Cerveja> cadastrar(@Valid @RequestBody CervejaDTO cervejaDTO) {
         Cerveja cerveja = cervejaService.salvar(cervejaDTO.cervejaMapper());
 
         return new ResponseEntity<>(cerveja, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cerveja> atualizar(@PathVariable Long id, @RequestBody Cerveja cerveja) {
+        cervejaService.atualizar(cerveja, id);
+        return ResponseEntity.ok(cerveja);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cerveja> remover(@PathVariable Long id) {
+        cervejaService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
