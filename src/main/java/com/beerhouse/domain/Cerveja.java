a2ad89d018e232a8a0f86fd3d167931cfb080a22
Renan Devenz <renan.devenz@hotmail.com>
2020-10-28 16:48:29 -0300

    Alteração no contexto do domain

--- src/main/java/com/beerhouse/domain/Cerveja.java
+++ src/main/java/com/beerhouse/domain/Cerveja.java
@@ -1,36 +1,15 @@
 package com.beerhouse.domain;
 
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
-@NoArgsConstructor
-@Entity
-@Table(name = "TB_CERVEJAS")
-public class Cerveja implements Serializable {
+@Builder
+public class Cerveja {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @NotBlank
-    @Column(name = "MARCA", unique = true)
     private String marca;
-
-    @NotBlank
-    @Column(name = "TIPO")
     private String tipo;
-
-    @NotNull
-    @Column(name = "PRECO")
     private BigDecimal preco;
-
-    public Cerveja(String marca, String tipo, BigDecimal preco) {
-    }
 }
