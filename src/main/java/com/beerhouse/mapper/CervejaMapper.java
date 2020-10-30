5edf28d74d6c324c1084baeca254cacb2ccf2503
Renan Devenz <renan.devenz@hotmail.com>
2020-10-30 10:02:51 -0300

    Add log

--- src/main/java/com/beerhouse/mapper/CervejaMapper.java
+++ src/main/java/com/beerhouse/mapper/CervejaMapper.java
@@ -2,10 +2,13 @@ package com.beerhouse.mapper;
 
 import com.beerhouse.domain.Cerveja;
 import com.beerhouse.entity.CervejaEntity;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CervejaMapper {
 
     public static CervejaEntity convert(Cerveja cerveja) {
+        log.info("Construção do Mapper");
         return CervejaEntity.builder()
                 .marca(cerveja.getMarca())
                 .tipo(cerveja.getTipo())
