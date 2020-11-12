dc6c131ba0567610a63ebc27503068fc261b1899
Renan Devenz <renan.devenz@hotmail.com>
2020-11-12 09:19:33 -0300

    Add controle de transação nos métodos de persistência

--- src/main/java/com/beerhouse/service/CervejaService.java
+++ src/main/java/com/beerhouse/service/CervejaService.java
@@ -3,13 +3,17 @@ package com.beerhouse.service;
 import com.beerhouse.entity.CervejaEntity;
 import com.beerhouse.repository.CervejaRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class CervejaService {
 
@@ -25,12 +29,14 @@ public class CervejaService {
                 .orElseThrow(() -> new IllegalArgumentException("Produto inexistente")));
     }
 
+    @Transactional
     public CervejaEntity salvar(CervejaEntity cervejaEntity) {
         Optional<CervejaEntity> cervejaExiste = cervejaRepository.findByMarca(cervejaEntity.getMarca());
 
         return cervejaRepository.save(cervejaEntity);
     }
 
+    @Transactional
     public CervejaEntity atualizar(CervejaEntity cervejaEntity, Long id) {
         if (cervejaRepository.findById(id).isPresent()) {
             cervejaEntity.setId(id);
@@ -38,6 +44,7 @@ public class CervejaService {
         } throw new IllegalArgumentException("Não existe um produto para atualizar");
     }
 
+    @Transactional
     public void deletar(Long id) {
         if (cervejaRepository.findById(id).isEmpty()) {
             cervejaRepository.deleteById(id);
