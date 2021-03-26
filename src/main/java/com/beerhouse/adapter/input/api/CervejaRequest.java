05a2a970a5953b4cfab8937a3978fdf792387ff2
Renan Devenz <renan.devenz@hotmail.com>
2021-03-26 11:26:58 -0300

    Criação do dto para mapeamento do request

--- src/main/java/com/beerhouse/adapter/input/api/CervejaRequest.java
+++ src/main/java/com/beerhouse/adapter/input/api/CervejaRequest.java
@@ -1,19 +1,15 @@
-package com.beerhouse.application.domain;
+package com.beerhouse.adapter.input.api;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-@Data
+@Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Cerveja {
+public class CervejaRequest {
 
     @NotBlank
     private String marca;
